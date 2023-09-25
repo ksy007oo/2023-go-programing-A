0c44d7e8b8bd51fc91dd63b3dd692820568366ca
gsy041230 <gsy041230@gmail.com>
2023-09-25 15:20:07 +0900

    week05 v0.9

--- week05/main.go
+++ week05/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,13 +14,21 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	//inputScore := reader.ReadString('\n')    //1 variable but reader.ReadString returns 2 values
 	//inputScore, err := read.ReadString('\n') // err declared and not used
-	inputScore, err := reader.ReadString('\n')
+	inputScoreString, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	if inputScore >= 90 { //mismatched types string and untyped int
+
+	inputScoreString = strings.TrimSpace(inputScoreString)      // 공란 제거 ( 띄어쓰기 제거 )
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // 실제 형변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if inputScore >= 90 {
 		grade := "A grade!"
 	} else {
 		grade := " BCDE grade"
 	}
+	fmt.Println("You got " + grade)
 }
