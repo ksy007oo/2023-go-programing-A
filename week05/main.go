af355969c07e14adc60d7ebe4263e8b1a9521868
gsy041230 <gsy041230@gmail.com>
2023-09-25 14:41:23 +0900

    week05 v0.5

--- week05/main.go
+++ week05/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	HotSpurs := "hm ? j madi?"
-	replacePlayer := strings.NewReplacer("?", "son")
-	player := replacePlayer.Replace(HotSpurs)
-	fmt.Println(player)
+	fmt.Print("Input Score : ")
+	reader := bufio.NewReader(os.Stdin)
+	//inputScore := reader.ReadString('\n')    //1 variable but reader.ReadString returns 2 values
+	//inputScore, err := read.ReadString('\n') // err declared and not used
+	inputScore, _ := reader.ReadString('\n') //option 1
+	fmt.Println(inputScore)
 }
