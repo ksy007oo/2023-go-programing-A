01b91c12b65dae052de69ef24d488da6cf2d40d0
gsy041230 <gsy041230@gmail.com>
2023-09-25 15:12:46 +0900

    week05 v0.8

--- week05/main.go
+++ week05/main.go
@@ -12,9 +12,13 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	//inputScore := reader.ReadString('\n')    //1 variable but reader.ReadString returns 2 values
 	//inputScore, err := read.ReadString('\n') // err declared and not used
-	inputScore, err := reader.ReadString('\n') //option 2
+	inputScore, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(inputScore)
+	if inputScore >= 90 { //mismatched types string and untyped int
+		grade := "A grade!"
+	} else {
+		grade := " BCDE grade"
+	}
 }
