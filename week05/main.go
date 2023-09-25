466c73a01b58967368f6165a54e3a4a62ec5c02a
gsy041230 <gsy041230@gmail.com>
2023-09-25 15:34:58 +0900

    week05 v1.1

--- week05/main.go
+++ week05/main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Input Score : ")
-	reader := bufio.NewReader(os.Stdin)
-	//inputScore := reader.ReadString('\n')    //1 variable but reader.ReadString returns 2 values
-	//inputScore, err := read.ReadString('\n') // err declared and not used
-	inputScoreString, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	inputScoreString = strings.TrimSpace(inputScoreString)      // 공란 제거 ( 띄어쓰기 제거 )
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // 실제 형변환
-	if err != nil {
-		log.Fatal(err)
-	}
-	var grade string
-	if inputScore >= 90 {
-		grade = "A grade!"
-	} else {
-		grade = "under A grade"
-	}
-	fmt.Println("You got " + grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
