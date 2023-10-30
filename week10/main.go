2eb713fe6fd83477703617b4b1c7554dec668bcd
gsy041230 <gsy041230@gmail.com>
2023-10-30 14:47:47 +0900

    week10 v2.1

--- week10/main.go
+++ week10/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "week10/src/greeting"
+import (
+	"fmt"
+	"week10/src/greeting"
+	"week10/src/mymath"
+)
 
 func main() {
+	fmt.Println(mymath.MyPower(2, 9))
+	fmt.Println(mymath.MyAbs(-99))
+	fmt.Println(mymath.MyAbs(17))
 	greeting.Hello()
 	greeting.Hi()
 }
