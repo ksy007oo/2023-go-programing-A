2b88aee1e52a9a25be4800bfe2ea41e8bb9aae46
gsy041230 <gsy041230@gmail.com>
2023-11-13 14:30:14 +0900

    week12 v3.1

---
+++ week12/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var s []int
+	// s = make([]int, 3)
+
+	// s := make([]int, 5) // 단축 연산자 make 함수 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
+
+	s := []int{0, 0, 0, 0, 0} // 슬라이스 리터럴 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+	s[4] = 99
+	s[2] = 91
+
+	for _, value := range s {
+		fmt.Println(value)
+	}
+
+	copyS := s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
+
+	test := [3]string{"inha", "go", "student"} // 배열 리터럴 이용해 test 배열 생성
+	// testS := test[0:4] // invalid argument: index 4 out of bounds [0:4]
+	testS := test[0:2]
+	fmt.Println(len((testS)))
+}
