0962c37317d775941f56994fd345b45f662e0117
gsy041230 <gsy041230@gmail.com>
2023-11-27 14:19:38 +0900

    week14 v4.4.

--- week14/main.go
+++ week14/main.go
@@ -6,6 +6,7 @@ func main() {
 	var games map[int]string
 	games = make(map[int]string)
 
+	// append ( 추가 )
 	games[456] = "성기훈"
 	games[218] = "박해수"
 	games[067] = "강새벽"
@@ -16,4 +17,11 @@ func main() {
 	for _, v := range games {
 		fmt.Println(v)
 	}
+	games[101] = "장덕수" // update ( 업데이트 )
+	delete(games, 199) // delete ( 삭제 )
+
+	for k, v := range games {
+		fmt.Println(k, v)
+	}
+
 }
